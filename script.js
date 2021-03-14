548464c3ec5da361ba09f507c4b48d1cf25144f5
Chelsea Nicholls <chelseanicholls95@live.co.uk>
2021-03-14 13:15:43 +0000

    added comments with what each section will do

--- script.js
+++ script.js
@@ -11,14 +11,27 @@ function writePassword() {
 
 // Declare a new function on line 3
 function generatePassword() {
-	// Create a variable to store our generated password
-	var password = ''
+  // Create a variable to store our generated password
+  var password = "";
 
-	// Logic would go here
-	...
+  // Prompt user to insert password length between 8 and 128 characters
 
-	// Return our created password
-	return password;
+  // Validate length of user input
+
+  // Confirm if the user wants lowercase letters
+
+  // Confirm if the user wants uppercase letters
+
+  // Confirm if the user wants special characters
+
+  // Confirm if the user wants numbers
+
+  // Validate that at least one of the above options have been selected
+
+  // Generate password
+
+  // Return our created password
+  return password;
 }
 
 // Add event listener to generate button
