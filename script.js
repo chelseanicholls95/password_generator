e312fe4ed57343b2b64494d25aa5d206ee3e474f
Chelsea Nicholls <chelseanicholls95@live.co.uk>
2021-03-17 18:11:04 +0000

    refactored the code

--- script.js
+++ script.js
@@ -8,65 +8,86 @@ function writePassword() {
 
   passwordText.value = password;
 }
-const isValidLength = function (number) {
-  return number >= 8 && number <= 128;
-};
-
-const isValidOption = function (lowercase, uppercase, special, numbers) {
-  return lowercase || uppercase || special || numbers;
-};
-
-const getRandomOption = function (array) {
-  // check length of array
-  const length = array.length;
-
-  // choose random index from the array
-  const randomOption = Math.floor(Math.random() * length);
-
-  // return the index number
-  return randomOption;
-};
-
-const getRandomCharacter = function (array) {
-  // check length of array
-  const length = array.length;
-
-  // choose a random index from that array
-  const randomCharacter = Math.floor(Math.random() * length);
-
-  // access character of that index number
-  const character = array[randomCharacter];
-
-  // return the character
-  return character;
-};
-
-// Arrays
-
-//prettier-ignore
-const lowerCaseArray = [
-    "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z",
-  ];
-
-// prettier-ignore
-const upperCaseArray = [
-    "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z",
-  ];
-
-// prettier-ignore
-const specialCharsArray = [
-    "!", "#", "$", "%", "!", "&", ")", "(", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[", "\\", "]", "^", "_", "{", "|", "}", "~", '"', "'", "`",
-  ];
-
-const numbersArray = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
-
-const optionsArray = [];
 
 // Declare a new function on line 3
 function generatePassword() {
   // Create a variable to store our generated password
-  var password = "";
+  let password = "";
 
+  // Functions
+  const isValidLength = function (number) {
+    return number >= 8 && number <= 128;
+  };
+
+  const isValidOption = function (lowercase, uppercase, special, numbers) {
+    return lowercase || uppercase || special || numbers;
+  };
+
+  const createOptionsArray = function () {
+    if (lowerCaseLetters) {
+      optionsArray.push(lowerCaseArray);
+    }
+
+    if (upperCaseLetters) {
+      optionsArray.push(upperCaseArray);
+    }
+
+    if (specialCharacters) {
+      optionsArray.push(specialCharsArray);
+    }
+
+    if (numbers) {
+      optionsArray.push(numbersArray);
+    }
+  };
+
+  const getRandomOption = function (array) {
+    // check length of array
+    const length = array.length;
+
+    // choose random index from the array
+    const randomOption = Math.floor(Math.random() * length);
+
+    // return the index number
+    return randomOption;
+  };
+
+  const getRandomCharacter = function (array) {
+    // check length of array
+    const length = array.length;
+
+    // choose a random index from that array
+    const randomCharacter = Math.floor(Math.random() * length);
+
+    // access character of that index number
+    const character = array[randomCharacter];
+
+    // return the character
+    return character;
+  };
+
+  // Arrays
+
+  //prettier-ignore
+  const lowerCaseArray = [
+      "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z",
+    ];
+
+  // prettier-ignore
+  const upperCaseArray = [
+      "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z",
+    ];
+
+  // prettier-ignore
+  const specialCharsArray = [
+      "!", "#", "$", "%", "!", "&", ")", "(", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[", "\\", "]", "^", "_", "{", "|", "}", "~", '"', "'", "`",
+    ];
+
+  const numbersArray = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+
+  const optionsArray = [];
+
+  // Function instructions
   // Prompt user to insert password length
   const passwordLengthInput = prompt(
     "How many characters would you like the password to be?"
@@ -98,7 +119,7 @@ function generatePassword() {
 
   const numbers = confirm("Do you want to include numbers in the password?");
 
-  // Validate that at least one of the above options have been selected
+  // Validate that at least one of the above options have been selected and push selected options into the options array
   if (
     isValidOption(
       lowerCaseLetters,
@@ -107,27 +128,10 @@ function generatePassword() {
       numbers
     )
   ) {
-    // Continue
+    createOptionsArray();
   } else {
     alert("Please choose at least one option.");
     return;
-  }
-
-  // Add selected options to an array
-  if (lowerCaseLetters) {
-    optionsArray.push(lowerCaseArray);
-  }
-
-  if (upperCaseLetters) {
-    optionsArray.push(upperCaseArray);
-  }
-
-  if (specialCharacters) {
-    optionsArray.push(specialCharsArray);
-  }
-
-  if (numbers) {
-    optionsArray.push(numbersArray);
   }
 
   // Generate password
@@ -137,9 +141,9 @@ function generatePassword() {
 
     // Choose a random character from the chosen option
     const character = getRandomCharacter(optionsArray[randomOption]);
-    // password = password.concat(character);
+
+    // Add random character to the password string
     password += character;
-    console.log(password);
   }
 
   // Return our created password
